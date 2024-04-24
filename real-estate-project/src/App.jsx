8f60e8ba16824e2ccef77f27d43320753eccba55
akashbhatiaA <155867535+akashbhatiaA@users.noreply.github.com>
2024-04-24 10:59:59 +0000

    real-estate-project

--- real-estate-project/src/App.jsx
+++ real-estate-project/src/App.jsx
@@ -1,30 +1,68 @@
-import './layout.scss'
-import Navbar from './components/navbar/Navbar.jsx'
-import HomePage from './routes/homepage/HomePage.jsx'
 
+import Navbar from './components/navbar/Navbar.jsx';
+import HomePage from  './routes/homepage/HomePage.jsx';
+import ListPage from './routes/listPage/listPage.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
+import Layout from './routes/layout/layout.jsx';
+import SinglePage from './routes/singlePage/singlePage.jsx';
+import ProfilePage from './routes/profilePage/profilePage.jsx';
+import About from './routes/About/About.jsx';
 function App() {
   
-
+    const router = createBrowserRouter([
+      {
+        path: "/",
+        element: <Layout />,
+        children: [
+          {
+            path: "/",
+            element: <HomePage />
+          },
+          {
+            path: "/list",
+            element: <ListPage />
+          },
+          {
+            path: "/:id",
+            element: <SinglePage />
+          },
+          {
+            path: "/profile",
+            element: <ProfilePage />
+          },
+          {
+            path: "/about",
+            element: <About />
+          }
+        ]
+      },
+     
+    ])
   return (
   
-      <div className='layout'>
+      // <div className='layout'>
 
-        <div className="navbar">
+      //   <div className="navbar">
 
-       <Navbar />
+      //  <Navbar />
 
-        </div>
+      //   </div>
 
-        <div className="content">
+      //   <div className="content">
 
-          <HomePage />
+      //     <HomePage />
 
-        </div>
-      </div>
+      //   </div>
+      // </div>
       
-
+    <RouterProvider router = {router} />
     
-  )
+  );
 }
 
 export default App
